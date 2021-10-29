ab9e0570d5aefb23f3d57a8c34ef863ec1c4ac29
halima254 <halima.yahya@student.moringaschool.com>
2021-10-29 15:28:57 +0300

    add profile model

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -5,13 +5,62 @@ from tinymce.models import HTMLField
 # Create your models here.
 
 #creating models file
-class Project(midels.Model):
+c
+
+
+class Project(models.Model):
     title = models.Charfield(max_length =50, blank=True)
-    image = models.ImageField(upload_to'projectimg/'),default = 'NO IMAGE')
+    image = models.ImageField(upload_to='projectimg/'),default = 'NO IMAGE')
     description = HTMLField()
     link = models.URLField(blank=True)
     User = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
     profile = models.ForeignKey(Profile, null = True, on_delete = models.CASCADE)
-    timestamp = models.DateTimeFiels(auto_now_add = True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def  avg_design(self):
+        design_reviews = list(map(lambda x:x.design, self.review_set.all()))
+        return np.mean(design_reviews)
+    
+    def avg_content(self):
+        content_reviews = list(map(lambda x: x.content, self.review_set.all()))
+        return np.mean(usability_reviews)
+    def __str__(Self):
+        return self.title
+    
+    @classmethod
+    def project_by_id(cls,id):
+        project = Project.objects.filter(id = id)
+        
+        return project
+    
+    @classmethod
+    def get_projects(cls):
+        projects = Project.objects.all()
+        return projects
     
     
+class Profile(models.Model):
+    photo = models.ImageField(upload_to ='profile/',default ='NO PROFILE')
+    bio = HTMLField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name ='profile',primary_key=True)  
+    contact = models.Charfield(max_length=50,blank=True)
+    timestamp = models.DateTimeField(default=timezone.now)  
+    
+    
+    
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+@receiver(post_save,sender=User)
+def save_user_profile(sender,instance, **kwargs):
+    instance.profile.save()
+    
+def __str__(self):
+    return self.user.username
+
+@classmethod 
+def filter_by_id(cls,id):
+    profile = Profile.objects.filter(user = id).first()
+    return profile          
