d0f8b2622653563e036eaccf3f80a2d69b910018
halima254 <halima.yahya@student.moringaschool.com>
2021-10-30 13:19:51 +0300

    configure db to settings

--- awards/settings.py
+++ awards/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'awards',
+        'USER':'moringa',
+        'PASSWORD':'access', 
     }
 }
 
